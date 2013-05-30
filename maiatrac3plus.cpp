64c8e4022f4d1b86238c89989011f00bd0a4693c
Emu Libraries <emulibraries@yahoo.com>
2013-05-30 14:42:03 +0200

    VS 2010 build setup

---
+++ maiatrac3plus.cpp
@@ -0,0 +1,26 @@
+// maiatrac3plus.cpp : Defines the exported functions for the DLL application.
+//
+
+#include "stdafx.h"
+#include "maiatrac3plus.h"
+
+#include "include/MaiAT3PlusFrameDecoder.h"
+
+extern "C" void* Atrac3plusDecoder_openContext()
+{
+	return new MaiAT3PlusFrameDecoder;
+}
+
+extern "C" int Atrac3plusDecoder_closeContext(void* context)
+{
+	MaiAT3PlusFrameDecoder *frame_decoder =(MaiAT3PlusFrameDecoder*)context;
+	if (frame_decoder)
+		delete frame_decoder;
+	return 0;
+}
+
+extern "C" int Atrac3plusDecoder_decodeFrame(void* context, void* inbuf, int inbytes, int* channels, void** outbuf)
+{
+	MaiAT3PlusFrameDecoder *frame_decoder = (MaiAT3PlusFrameDecoder*)context;
+	return frame_decoder->decodeFrame((Mai_I8*)inbuf, inbytes, channels, (Mai_I16**)outbuf);
+}
